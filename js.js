7fb5718d7e09f6daf325f1fab5ba35e14b441894
Jpedroaugusto <pedroagusuto131204@gmail.com>
2022-11-09 18:21:13 -0300

    interface

--- js.js
+++ js.js
@@ -1,23 +1,51 @@
-let screen = document.getElementById('screen');
+const screen = document.getElementById('screen');
+const dashboard = document.getElementById('dashboard');
 
-for(let i = 1; i <= 4; i++) {
-    screen.innerHTML += `<div onclick="flipcard(${i})" class="card backCard" id="card${i}"></div>`
-    screen.innerHTML += `<div onclick="flipcard(${i},'t')" class="card backCard" id="scard${i}"></div>`
-}
-const cards = document.querySelectorAll('.card')
+window.addEventListener('load', () => {
+    let div = "";
+    div = `<div>`;
+    div += `<h1>Jogo da Memoria</h1>`;
+    div += `<div id="dashboard"><input id="player" placeholder="Digite seu nome"><button onclick="startGame()">start</button></div>`;
+    div += `</div>`;
 
-let tclick = 0;
+    screen.innerHTML = div;
+})
 
-function flipcard(card,s) {
-    if(s != undefined) {
-        document.getElementById('scard'+card).classList.remove("backCard");
-        document.getElementById('scard'+card).classList.add("frontCard");
-    } else {
-        document.getElementById('card'+card).classList.add("frontCard");
-        document.getElementById('card'+card).classList.remove("backCard");
+function startGame() {
+    let player = document.getElementById("player").value;
+    if(player) {
+        screen.innerHTML = "";
+        screen.innerHTML += `<div id="dashplayer"><p>Nome do Jogador: ${player}</div>`
+        screen.innerHTML += `<div id="game"></div>`
+
+        let cards = Array();
+
+        for(let i = 1; i <= 4; i++) {
+            cards.push(`<div onclick="flipcard(${i})" class="card" id="card${i}"></div>`);
+            cards.push(`<div onclick="flipcard(${i},'s')" class="card" id="scard${i}"></div>`);
+        }
+        
+        let game = document.getElementById("game");
+
+        game.innerHTML = ""
+        cards.forEach(card => {
+            game.innerHTML += card;
+        });
+
+        cards = document.querySelectorAll('.card');
+        embaralhar(cards)
     }
-    click(card);
 }
+
+function flipcard(index,duplicate) {
+    if(duplicate != undefined) {
+        document.getElementById('scard'+index).classList.add("frontCard");
+    } else {
+        document.getElementById('card'+index).classList.add("frontCard");
+    }
+    click(index);
+}
+let tclick = 0;
 
 function click(card) {
     if(tclick !=1) {
@@ -30,17 +58,26 @@ function click(card) {
         
         let interval;
         interval = setInterval(() => {
-            let divs = document.querySelectorAll('.card')
+            let divs = document.querySelectorAll('.card');
+
             for(let i = 0; i < divs.length; i++) {
                 divs[i].classList.remove("frontCard");
             }
             clearInterval(interval)
-        }, 1000);
+        }, 800);
         tclick = 0;
     }
 }
 
-cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-})
+function vitoria() {
+    if(document.querySelectorAll('.card').length == document.querySelectorAll('.selected').length) {
+        alert("PARABENS VOCÊ GANHOU")
+    }
+}
+
+function embaralhar(cards) {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+}
